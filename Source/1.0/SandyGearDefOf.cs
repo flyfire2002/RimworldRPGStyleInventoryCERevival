655557b210186fae2bc8f9baed3339c654cd8800
Huajie Wu <wuhuajie.flyfire2002@gmail.com>
2020-02-26 00:15:45 -0800

    Untabify all source

--- Source/1.0/SandyGearDefOf.cs
+++ Source/1.0/SandyGearDefOf.cs
@@ -4,17 +4,17 @@ using RimWorld;
 
 namespace Sandy_Detailed_RPG_Inventory
 {
-	[DefOf]
+    [DefOf]
     public static class Sandy_Gear_DefOf
     {	
-    	public static BodyPartGroupDef Teeth;
-    	public static BodyPartGroupDef Mouth;
-    	public static BodyPartGroupDef Neck;
-    	public static BodyPartGroupDef Shoulders;
-    	public static BodyPartGroupDef Arms;
-    	public static BodyPartGroupDef Hands;
-    	public static BodyPartGroupDef Waist;
-    	public static BodyPartGroupDef Feet;
+        public static BodyPartGroupDef Teeth;
+        public static BodyPartGroupDef Mouth;
+        public static BodyPartGroupDef Neck;
+        public static BodyPartGroupDef Shoulders;
+        public static BodyPartGroupDef Arms;
+        public static BodyPartGroupDef Hands;
+        public static BodyPartGroupDef Waist;
+        public static BodyPartGroupDef Feet;
         //This was added for Jewelry
         //Two defs file was added, they are in Defs\Jewelry_compat
         public static BodyPartGroupDef Ears;
